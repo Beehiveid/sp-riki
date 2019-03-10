8ee392691cc27d3deca9f8ba1126546505b056f3
Febrinanda <febriinanda@gmail.com>
2019-03-10 15:55:57 +0700

    JPA Auditing OK

--- src/main/java/com/beehive/riki/common/AuditorBase.java
+++ src/main/java/com/beehive/riki/common/AuditorBase.java
@@ -2,13 +2,13 @@ package com.beehive.riki.common;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
-//todo JPA auditing
-@EntityListeners(EntityListeners.class)
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public class AuditorBase implements Serializable {
     @Id
